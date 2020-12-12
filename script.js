f292e5a464904e0deb2bc0fed44f46aaa8f1e2a1
alessiaskia <a.scaccia@interface3.be>
2020-12-12 22:36:45 +0100

    changes before media-queries

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 import './style.scss';
 // import $ from 'jquery';
 // import axios from 'axios';
-import { tradMenu } from './trad';
+import { tradMenu } from './src/trad';
 
 const nav = document.querySelector('.nav-bar');
 const langButtons = document.querySelectorAll('.language-button');
-const closeButton = document.querySelector('.close');
 const openButton = document.querySelector('.open');
 
 // --- changement de langue --- //
@@ -19,22 +18,23 @@ for (langButton of langButtons) {
   langButton.addEventListener('click', (e) => {
     console.log(e.currentTarget.id);
     currentLanguage = e.currentTarget.id;
-    // console.log(tradMenu[currentLanguage].about);
     menu = `<div class="nav-bar-links"><ul class="d-flex"><li><a href="#contact" class="nav-link contact">contact</a></li><li><a href="#background" class="nav-link background">${tradMenu[currentLanguage].background}</a></li><li><a href="#translation" class="nav-link translation">${tradMenu[currentLanguage].translation}</a></li><li><a href="#web-dev" class="nav-link web-dev">${tradMenu[currentLanguage].web}</a></li><li><a href="#about" class="nav-link about">${tradMenu[currentLanguage].about}</a></li></ul></div>`;
-    nav.innerHTML = `${menu}<div class="close"><i class="fas fa-window-close"></i></div>`;
+    nav.innerHTML = `<div class="close"><i class="fas fa-times"></i></div>${menu}`;
   });
 }
 
 // --- cache/montre barre nav --- //
 function menuHide() {
   document.body.addEventListener('click', (e) => {
-    if (e.target.matches('.fa-window-close')) {
+    if (e.target.matches('.fa-times')) {
       console.log('close ok');
       nav.classList.add('cache');
+      nav.classList.add('show');
       openButton.classList.remove('cache');
-    } else if (e.target.matches('.fa-arrow-right')) {
+    } else if (e.target.matches('.fa-bars')) {
       console.log('open ok');
       nav.classList.remove('cache');
+      nav.classList.add('show');
       openButton.classList.add('cache');
     }
   });
